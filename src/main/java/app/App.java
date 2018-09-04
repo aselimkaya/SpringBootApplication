e1e0e86d6eb901b93c505f72acac1d561167be65
selimk <selim.kaya@comodo.com>
2018-09-04 17:24:48 +0300

    Add welcome page.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,10 +1,12 @@
 package app;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication @ComponentScan(basePackages = {"app.*"})
 public class App {
     public static void main(String[] args) {
-
+        SpringApplication.run(App.class, args);
     }
 }
